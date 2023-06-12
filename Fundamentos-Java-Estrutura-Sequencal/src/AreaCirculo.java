44cf6220df1b8f7b3dfdfbf989642a543a2d2420
Renan Mota <renanmota1996@gmail.com>
2023-06-12 14:26:14 -0300

    Introdução a estruturas condicionais

--- Fundamentos-Java-Estrutura-Sequencal/src/AreaCirculo.java
+++ Fundamentos-Java-Estrutura-Sequencal/src/AreaCirculo.java
@@ -1,42 +1,4 @@
-import java.util.Locale;
 import java.util.Scanner;
-public class AreaCirculo{
-    public static void main (String[] args){
-        Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Iremos calcular a área de um circulo, digite o raio: ");
-        double r = sc.nextDouble();
-        double area= Math.PI*Math.pow(r,2);
-        System.out.printf("Levando em conta que o raio é %f e pí é aproximadamnte 3.1415\n", r);
-        System.out.println("A area é: "+area);
-
-
-        sc.close();
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*import java.util.Scanner;
 
 public class AreaCirculo {
     public static void main(String[] args){
@@ -50,4 +12,3 @@ public class AreaCirculo {
         sc.close();
     }
 }
-*/
