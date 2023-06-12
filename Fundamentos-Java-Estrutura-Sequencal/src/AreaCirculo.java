a4837fd5481bd68d08c34b6db719081368287004
Renan Mota <renanmota1996@gmail.com>
2023-06-12 12:11:23 -0300

    Atualização

--- Fundamentos-Java-Estrutura-Sequencal/src/AreaCirculo.java
+++ Fundamentos-Java-Estrutura-Sequencal/src/AreaCirculo.java
@@ -1,4 +1,42 @@
+import java.util.Locale;
 import java.util.Scanner;
+public class AreaCirculo{
+    public static void main (String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Iremos calcular a área de um circulo, digite o raio: ");
+        double r = sc.nextDouble();
+        double area= Math.PI*Math.pow(r,2);
+        System.out.printf("Levando em conta que o raio é %f e pí é aproximadamnte 3.1415\n", r);
+        System.out.println("A area é: "+area);
+
+
+        sc.close();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import java.util.Scanner;
 
 public class AreaCirculo {
     public static void main(String[] args){
@@ -12,3 +50,4 @@ public class AreaCirculo {
         sc.close();
     }
 }
+*/
